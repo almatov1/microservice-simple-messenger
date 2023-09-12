2e2ae4c2bdfd0aa586c74447240bf592a64cb7b3
almatov1 <almatovalshin@gmail.com>
2023-09-12 12:20:01 +0500

    totally updated

---
+++ git-total-push.sh
@@ -0,0 +1,33 @@
+cd module/client
+git add .
+git commit -m 'updated'
+git push
+cd ../..
+
+cd module/controller
+git add .
+git commit -m 'updated'
+git push
+cd ../..
+
+cd module/discovery
+git add .
+git commit -m 'updated'
+git push
+cd ../..
+
+cd module/gateway
+git add .
+git commit -m 'updated'
+git push
+cd ../..
+
+cd module/message
+git add .
+git commit -m 'updated'
+git push
+cd ../..
+
+git add .
+git commit -m 'totally updated'
+git push
